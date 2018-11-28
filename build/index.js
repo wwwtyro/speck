cfabe23e748df43d048f6cf94da2ffb9245b6594
Shammamah Hossain <shammamah.hossain@gmail.com>
2018-11-28 14:04:26 -0500

    Remove unnecessary files, update package.json, and configure webpack and index.js.

---
+++ build/index.js
@@ -0,0 +1,11 @@
+const speckRenderer = require('../src/renderer.js');
+const speckSystem = require('../src/system.js');
+const speckView = require('../src/view.js');
+const speckInteractions = require('../src/interactions.js');
+
+export {
+    speckRenderer,
+    speckSystem,
+    speckView,
+    speckInteractions
+}
