95a865f87681a3a85fa7625b64f40d33958e9f37
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-01-04 15:31:56 -0500

    Include view presets in exports.

--- build/index.js
+++ build/index.js
@@ -2,10 +2,12 @@ const speckRenderer = require('../src/renderer.js');
 const speckSystem = require('../src/system.js');
 const speckView = require('../src/view.js');
 const speckInteractions = require('../src/interactions.js');
+const speckPresetViews = require('../src/presets.js');
 
 module.exports = {
     speckRenderer,
     speckSystem,
     speckView,
-    speckInteractions
+    speckInteractions,
+    speckPresetViews
 }
