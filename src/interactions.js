c5e2346b457eb925aab0436cbfbae4b653652820
Manfred Cheung <mj3cheun@uwaterloo.ca>
2019-05-25 13:31:12 -0400

    Rewrite interactions.js to be app implementation agnostic

--- src/interactions.js
+++ src/interactions.js
@@ -2,73 +2,69 @@
 
 var speckView = require("./view.js");
 
-module.exports = function(component, renderer, container) {
-    component.setState({
-        interactions: {
-            buttonDown: false,
-            lastX: 0.0,
-            lastY: 0.0
-        }
-    });
+module.exports = function(args) {
+    const {
+        scrollZoom = true,
+        container,
+
+        getRotation,
+        setRotation,
+
+        getZoom,
+        setZoom,
+
+        refreshView
+    } = args;
+
+    var interactions = {
+        buttonDown: false,
+        lastX: 0.0,
+        lastY: 0.0
+    };
 
     function mousedownFn(e) {
         if(e.button == 0) {
-            let tmp_interactions = component.state.interactions;
-            tmp_interactions.buttonDown = true;
-            tmp_interactions.lastX = e.clientX;
-            tmp_interactions.lastY = e.clientY;
-
-            component.setState({
-                interactions: tmp_interactions,
-                refreshView: true
-            });
+            interactions = {
+                buttonDown: true,
+                lastX: e.clientX,
+                lastY: e.clientY
+            };
         }
     }
     container.addEventListener("mousedown", mousedownFn);
 
     function mouseupFn(e) {
         if(e.button == 0) {
-            let tmp_interactions = component.state.interactions;
-            if(!tmp_interactions.buttonDown) {
+            if(!interactions.buttonDown) {
                 return;
             }
 
-            tmp_interactions.buttonDown = false;
-
-             component.setState({
-                interactions: tmp_interactions,
-                refreshView: false
-            });
+            interactions.buttonDown = false;
         }
     }
     window.addEventListener("mouseup", mouseupFn);
 
     function mousemoveFn(e) {
-        var tmp_interactions = component.state.interactions;
-        if(!tmp_interactions.buttonDown){
+        if(!interactions.buttonDown){
             return;
         }
 
-        var dx = e.clientX - tmp_interactions.lastX;
-        var dy = e.clientY - tmp_interactions.lastY;
+        var dx = e.clientX - interactions.lastX;
+        var dy = e.clientY - interactions.lastY;
         if(dx == 0 && dy == 0) {
             return;
         }
 
-        tmp_interactions.lastX = e.clientX;
-        tmp_interactions.lastY = e.clientY;
+        interactions.lastX = e.clientX;
+        interactions.lastY = e.clientY;
 
-        var view = Object.assign({}, component.props.view);
-        speckView.rotate(view, dx, dy);
+        var viewDummyObj = {
+            rotation: new Float32Array(getRotation())
+        };
+        speckView.rotate(viewDummyObj, dx, dy);
 
-        component.props.setProps({
-            view: view
-        });
-
-        component.setState({
-            interactions: tmp_interactions,
-            refreshView: true
-        });
+        setRotation(viewDummyObj.rotation);
+        refreshView();
     }
     window.addEventListener("mousemove", mousemoveFn);
 
@@ -76,18 +72,11 @@ module.exports = function(component, renderer, container) {
         // prevents the page from scrolling when using scroll wheel inside speck component
         e.preventDefault();
 
-        component.props.setProps({
-            view: Object.assign(
-                component.props.view,
-                {zoom: component.props.view.zoom * (e.deltaY < 0 ? 1/0.9 : 0.9)}
-            )
-        });
+        setZoom(getZoom() * (e.deltaY < 0 ? 1/0.9 : 0.9));
+        refreshView();
 
-        component.setState({
-            refreshView: true
-        });
     }
-    if(component.props.scrollZoom) {
+    if(scrollZoom) {
         container.addEventListener("wheel", wheelFn);
     }
 
